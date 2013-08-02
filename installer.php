d21d5092459952ba1db4ab584511fee262f5e759
NicoNet <github@niconet2k.com>
2013-08-02 16:44:03 -0400

    Update installer.php

--- installer.php
+++ installer.php
@@ -1,1 +1,17 @@
-#
+<?php
+if(file_exists('install_done')) die('Install already done.');
+echo '<h1>Solar Empire Installer</h1>';
+echo '<p>Welcome to the Solar Empire Installer, we will need some information about your server to do initial configuration.</p>';
+echo '<p>First we need your MySQL database information, the database you list here needs to exist already, please create the database now if you haven''t already.';
+
+#database host
+#database name
+#database user name
+#database password
+
+#Authorization e-mail
+#Server Name
+#Address
+#...
+
+?>
